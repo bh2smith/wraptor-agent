1a91d08ee72e5c69f2e3bd48ceed16f9935d7b63
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-01-22 09:40:22 +0100

    WETH svg

--- src/app/api/ai-plugin/constants.ts
+++ src/app/api/ai-plugin/constants.ts
@@ -2,6 +2,6 @@ export const NAME = "Bitte Native Asset Wraptor";
 export const DESCRIPTION =
   "Agent API for transaction with WETH9. Prepares wrap and unwrap native asset transaction on any network";
 
-export const IMAGE = "weth.jpg";
+export const IMAGE = "weth.svg";
 export const INSTRUCTIONS =
   "Encodes transactions for wrapping and unwraping native assets on a given chainId. If the user wants to wrap or unwrap 'all' assets, set 'all' parameter to true and amount to 0.";
