46a3719e84d6dba9a2f5ffe139ec4167ecf67592
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-01-22 09:49:52 +0100

    *Change Title & Description (manifest update) (#5)

--- src/app/api/ai-plugin/constants.ts
+++ src/app/api/ai-plugin/constants.ts
@@ -1,7 +1,7 @@
-export const NAME = "Bitte Native Asset Wraptor";
+export const NAME = "Bitte WETH Wraptor";
 export const DESCRIPTION =
-  "Agent API for transaction with WETH9. Prepares wrap and unwrap native asset transaction on any network";
+  "Agent API for transacting with WETH9. Prepares wrap and unwrap native asset transaction on any network";
 
 export const IMAGE = "weth.svg";
 export const INSTRUCTIONS =
-  "Encodes transactions for wrapping and unwraping native assets on a given chainId. If the user wants to wrap or unwrap 'all' assets, set 'all' parameter to true and amount to 0.";
+  "Encodes transactions for wrapping and unwraping native assets on a given chainId. If the user wants to wrap or unwrap 'all' assets, set 'all' parameter to true and amount to 0. Does not ask for confirmation.";
