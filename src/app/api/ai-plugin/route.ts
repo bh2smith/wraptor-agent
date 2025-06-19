d9eb9ec71d80b1236a53b3eeafb36a67a75ca777
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-06-19 14:55:45 +0200

    Update Manifest

--- src/app/api/ai-plugin/route.ts
+++ src/app/api/ai-plugin/route.ts
@@ -22,6 +22,18 @@ export async function GET() {
         instructions: INSTRUCTIONS,
         tools: [{ type: "generate-evm-tx" }],
         image: `${url}/${IMAGE}`,
+        categories: ["wrappin'"],
+        chainIds: [
+          1, // Mainnet
+          10, // Optimism
+          56, // Binance Smart Chain (BSC)
+          137, // Polygon
+          8453, // Base (Coinbase L2)
+          42161, // Arbitrum One
+          42220, // CELO
+          43114, // Avalanche
+          81457, // Blast
+        ],
       },
     },
     paths: {
