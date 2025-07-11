dd008eaf45c7bc7336c9ffa5d4dfffa951d85f56
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-07-11 12:08:06 +0200

    Dynamically generated supported chainIds

--- src/app/api/ai-plugin/constants.ts
+++ src/app/api/ai-plugin/constants.ts
@@ -1,3 +1,5 @@
+import { wrappedMap } from "@/src/lib/static";
+
 export const NAME = "Bitte WETH Wraptor";
 export const DESCRIPTION =
   "Agent API for transacting with WETH9. Prepares wrap and unwrap native asset transaction on any network";
@@ -5,3 +7,5 @@ export const DESCRIPTION =
 export const IMAGE = "wraptor.png";
 export const INSTRUCTIONS =
   "Encodes transactions for wrapping and unwraping native assets on a given chainId. If the user wants to wrap or unwrap 'all' assets, set 'all' parameter to true and amount to 0. Does not ask for confirmation.";
+
+export const CHAIN_IDS = Object.keys(wrappedMap).map(Number);
